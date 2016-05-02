6653c8a028e41a87178c24df9362f1e8f344d856
joshuachen0 <joshuachen0@users.noreply.github.com>
2016-05-01 18:39:36 -0700

    will this fuckign work

--- dbal.js
+++ dbal.js
@@ -1,7 +1,31 @@
-displayDbal();
+/*displayDbal();
 
 
 function displayDbal() {
     startDate = new Date("March 17, 2016");
     endDate = new Date("June 11, 2016");
+}*/
+
+function ticker() {
+    var startDate = new Date("March 17, 2016");
+    var endDate = new Date("June 11, 2016");
+    var currentDate = new Date();
+    var quarterPeriod = endDate.getTime() - startDate.getTime();
+    var result = (1 - (currentDate.getTime() - startDate.getTime()) / quarterPeriod) * 475.00;
+    if (result < 0) {
+        result = 0.00;
+    }
+    document.getElementById("dballeft").innerHTML = "$" + result.toFixed(2);
+}
+
+endDate = new Date("June 11, 2016");
+cDate = new Date();
+safe = 0;
+while (cDate.getTime() != endDate.getTime()) {
+    cDate = new Date();
+    delay = new Date(cDate.getTime() + 1000);
+    ticker();
+    while (cDate.getTime() != delay.getTime()) {
+        cDate = new Date();
+    }
 }
