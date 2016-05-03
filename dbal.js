1a6e3bec544d609cfbea699a7473f766cbb34cd1
brickoflego <googlypantsroberteng@gmail.com>
2016-05-02 20:57:36 -0700

    Separated the files

--- dbal.js
+++ dbal.js
@@ -1,17 +1,9 @@
-/*displayDbal();
-
-
-function displayDbal() {
-    startDate = new Date("March 17, 2016");
-    endDate = new Date("June 11, 2016");
-}*/
-
 function ticker() {
     var startDate = new Date("March 17, 2016");
     var endDate = new Date("June 11, 2016");
     var currentDate = new Date();
-    var quarterPeriod = endDate.getTime() - startDate.getTime();
-    var result = (1 - (currentDate.getTime() - startDate.getTime()) / quarterPeriod) * 475.00;
+    var quarterLength = endDate.getTime() - startDate.getTime();
+    var result = (1 - (currentDate.getTime() - startDate.getTime()) / quarterLength) * 475.00;
     if (result < 0) {
         result = 0.00;
     }
@@ -19,14 +11,4 @@ function ticker() {
 }
 
 ticker();
-endDate = new Date("June 11, 2016");
-cDate = new Date();
-//while (cDate.getTime() != endDate.getTime()) {
-    cDate = new Date();
-    delay = new Date(cDate.getTime() + 1000);
-    ticker();
-    while (cDate.getTime() != delay.getTime()) {
-        cDate = new Date();
-    }
-//}
-
+setInterval(function(){ticker()}, 200)
