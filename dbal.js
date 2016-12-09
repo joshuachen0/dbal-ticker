d8cc7f2542ca9ce381201ef3e446034809b650e5
joshuachen0 <joshuachen0@users.noreply.github.com>
2016-12-08 20:56:35 -0800

    Edge case error -- end of term a day early

--- dbal.js
+++ dbal.js
@@ -1,31 +1,3 @@
-/*function ticker() {
-    var startDate = new Date("March 17, 2016");
-    var endDate = new Date("June 11, 2016");
-    var currentDate = new Date();
-    var quarterLength = endDate.getTime() - startDate.getTime();
-    var result = (1 - (currentDate.getTime() - startDate.getTime()) / quarterLength) * 475.00;
-    if (result < 0) {
-        result = 0.00;
-    }
-    document.getElementById("dballeft").innerHTML = "$" + result.toFixed(2);
-}*/
-
-/*var fileDisplayArea = document.getElementById('fileDisplayArea');
-function readDates(file) {
-	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", file, false);
-	rawFile.onreadystatechange = function () {
-		if(rawFile.readyState === 4) {
-			if(rawFile.status === 200 || rawFile.status == 0) {
-				var allText = rawFile.responseText;
-				fileDisplayArea.innerText = allText;
-			}
-		}
-	}
-	rawFile.send(null);
-}
-readDates("file:///home/josh/Documents/dbal-ticker/dates.txt");*/
-
 function ticker() {
     var termDates = [["9/26/16", "12/9/16"], ["1/4/17", "3/17/17"], ["4/3/17", "6/16/17"]];
     var term = 0;
@@ -38,7 +10,8 @@ function ticker() {
     var startDate = new Date(termDates[term][0]);
     var endDate = new Date(termDates[term][1]);
     var quarterLength = endDate.getTime() - startDate.getTime();
-    var result = (1 - (currentDate.getTime() - startDate.getTime()) / quarterLength) * 475.00;
+    var lengthOfDayInMs = 1000*60*60*24;
+    var result = (1 - (currentDate.getTime() - startDate.getTime()) / (quarterLength + lengthOfDayInMs)) * 475.00;
     if (result < 0) {
         result = 0.00;
     }
