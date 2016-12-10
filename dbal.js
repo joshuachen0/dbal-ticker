95e27793c028541ed8f0d1bc1133b12c66b1d2d1
joshuachen0 <joshuachen0@users.noreply.github.com>
2016-12-10 04:35:00 -0800

    Rewrote logic

--- dbal.js
+++ dbal.js
@@ -1,21 +1,23 @@
 function ticker() {
     var termDates = [["9/26/16", "12/9/16"], ["1/4/17", "3/17/17"], ["4/3/17", "6/16/17"]];
-    var term = 0;
-    if (term < 2) var nextStartDate = new Date(termDates[term+1][0]);
-    else var nextStartDate = new Date(termDates([term][1]));
-    var currentDate = new Date();
-    while (currentDate.getTime() >= nextStartDate.getTime()) {
-        term += 1;
-        nextStartDate = new Date(termDates[term+1][0]);
-    }
-    var startDate = new Date(termDates[term][0]);
-    var endDate = new Date(termDates[term][1]);
-    var quarterLength = endDate.getTime() - startDate.getTime();
     var lengthOfDayInMs = 1000*60*60*24;
-    var result = (1 - (currentDate.getTime() - startDate.getTime()) / (quarterLength + lengthOfDayInMs)) * 475.00;
-    if (result < 0) {
-        result = 0.00;
+    
+    var term = 1;
+    var currentDate = new Date();
+    var thisTerm = termDates[term-1];
+    var thisTermDates = [new Date(thisTerm[0]), new Date(thisTerm[1])];
+    while (term < 3 && currentDate.getTime() > (thisTermDates[1].getTime() + lengthOfDayInMs)) {
+        term += 1;
+        thisTerm = termDates[term-1];
+        thisTermDates = [new Date(thisTerm[0]), new Date(thisTerm[1])];
     }
+    
+    var startDate = thisTermDates[0];
+    var endDate = thisTermDates[1];
+    var quarterLength = endDate.getTime() - startDate.getTime();
+    var result = 0.00;
+    if (currentDate.getTime() >= startDate)
+        result = (1 - (currentDate.getTime() - startDate.getTime()) / (quarterLength + lengthOfDayInMs)) * 475.00;
     document.getElementById("dballeft").innerHTML = "$" + result.toFixed(2);
 }
 
